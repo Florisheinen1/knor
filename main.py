cc25036ac1f590ae16d9ac2ee3761d353b4be397
Floris Heinen <Floris.heinen1@gmail.com>
2024-01-26 23:22:35 +0100

    Rename compression profiler to optimization profiler

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from compression_profiler import *
+from optimization_profiler import *
 
 LAPTOP = False
 
 if LAPTOP:
-	do_tests(3, 20, 10, TestSize.Small)
+	third_try(3, 10, TestSize.Small)
 else:
-	do_tests(20, 120, 60, TestSize.Big)
+	third_try(20, 60, TestSize.Big)
